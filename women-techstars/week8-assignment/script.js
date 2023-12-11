00a0631fdf47bc297e8dace9237e09ed08327466
jameelahadejoke001@gmail.com <jameelahadejoke001@gmail.com>
2023-12-11 09:14:47 +0100

    Assignment

---
+++ women-techstars/week8-assignment/script.js
@@ -0,0 +1,7 @@
+function SingSong(){
+  console.log("Twinkle,twinkle, little star,");
+  console.log("How i wonder what you are,");
+  console.log("Up above the world so high,");
+  console.log("like a diamond in the sky");
+}
+SingSong();
